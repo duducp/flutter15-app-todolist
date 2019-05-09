616b52e5a22060f3c78549c4ccf9bd71b5dc270c
Carlos Eduardo <duducp2013@gmail.com>
2019-05-08 21:24:49 -0300

    Adicionada a ação e remover um item e desfaz a remoção caso o usuario queira

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final _toDoController = TextEditingController();
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
   @override
   void initState() {
@@ -99,6 +101,27 @@ class _HomeState extends State<Home> {
           });
         },
       ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          final snackbar = SnackBar(
+            content: Text('Tarefa \"${_lastRemoved['title']}\" removida'),
+            action: SnackBarAction(label: 'Desfazer', onPressed: () {
+              setState(() {
+                _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                _saveData();
+              });
+            }),
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snackbar);
+        });
+      },
     );
   }
 
